442c5f5a2e2e2dfb7504102fcd9e378f5caea7ab
kiztopia1 <62418642+kiztopia1@users.noreply.github.com>
2021-11-17 10:59:14 -0700

    aside style updated

--- src/components/Aside.vue
+++ src/components/Aside.vue
@@ -22,3 +22,18 @@ export default {
     name:"Aside"
 }
 </script>
+
+<style scoped>
+aside{
+    padding: 10px;
+    border-right: 1px solid #2c3e50;;
+    
+}
+ul{
+    padding: 0px;
+}
+li{
+    list-style: none;
+    padding:10px
+}
+</style>
